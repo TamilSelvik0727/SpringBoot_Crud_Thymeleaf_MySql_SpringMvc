a846121886991cc4938538c8b3fead8e9fe00da1
TamilSelvik0727 <111735176+TamilSelvik0727@users.noreply.github.com>
2023-08-11 11:46:25 +0530

    Add files via upload

---
+++ project1_Sb_Crud/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.demo.model.Employee;
+
+
+public interface EmployeeService {
+	
+	List<Employee> getAllEmployees();
+	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmployeeById(long id);
+	
+	//pagination
+	Page<Employee> findPaginated(int PageNo,int pageSize);
+		
+		
+	
+
+}
